ef9ee3ef7967a1cef415916b038c9b4dce2defc3
afonsocp35 <afonsopocas@gmail.com>
2022-10-07 15:51:50 +0100

    Another change to the hero

---
+++ src/main/java/ldts/Arena.java
@@ -0,0 +1,133 @@
+package ldts;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Arena {
+    private final Hero hero;
+
+    private int width;
+    private int height;
+
+    private List<Wall> walls;
+    private final List<Coin> coins;
+    private final List<Monster> monsters;
+
+    public Arena(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        hero = new Hero(width / 2, height / 2);
+        this.walls = createWalls();
+        this.coins = createCoins();
+        this.monsters = createMonsters();
+    }
+
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+        for (int i = 0; i < 5; i++)
+            coins.add(new Coin(random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1));
+        return coins;
+    }
+
+    private List<Monster> createMonsters() {
+        Random random = new Random();
+        ArrayList<Monster> monsters = new ArrayList<>();
+        for (int i = 0; i < 5; i++)
+            monsters.add(new Monster(random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1));
+        return monsters;
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+
+        return walls;
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#01579B"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+
+        hero.draw(graphics);
+
+        for (Wall wall : walls) wall.draw(graphics);
+
+        for (Coin coin : coins) coin.draw(graphics);
+
+        for (Monster monster : monsters) monster.draw(graphics);
+    }
+
+    public void processKey(KeyStroke key) {
+        if (key.getKeyType() == KeyType.ArrowUp) moveHero(hero.moveUp());
+        if (key.getKeyType() == KeyType.ArrowRight) moveHero(hero.moveRight());
+        if (key.getKeyType() == KeyType.ArrowDown) moveHero(hero.moveDown());
+        if (key.getKeyType() == KeyType.ArrowLeft) moveHero(hero.moveLeft());
+
+        retrieveCoins();
+
+        verifyMonsterCollisions();
+        moveMonsters();
+        verifyMonsterCollisions();
+    }
+
+    private void verifyMonsterCollisions() {
+        for (Monster monster : monsters)
+            if (hero.getPosition().equals(monster.getPosition())) {
+                System.out.println("You died!");
+                System.exit(0);
+            }
+    }
+
+    private void moveMonsters() {
+        for (Monster monster : monsters) {
+            Position monsterPosition = monster.move();
+            if (canHeroMove(monsterPosition))
+                monster.setPosition(monsterPosition);
+        }
+    }
+
+    private void retrieveCoins() {
+        for (Coin coin : coins)
+            if (hero.getPosition().equals(coin.getPosition())) {
+                coins.remove(coin);
+                break;
+            }
+    }
+
+    public void moveHero(Position position) {
+        if (canHeroMove(position)) {
+            hero.setPosition(position);
+        }
+    }
+
+    private boolean canHeroMove(Position position) {
+        if (position.getX() < 0) return false;
+        if (position.getX() > width - 1) return false;
+        if (position.getY() < 0) return false;
+        if (position.getY() > height - 1) return false;
+
+        for (Wall wall : walls)
+            if (wall.getPosition().equals(position)) return false;
+
+        return true;
+    }
+}
---
+++ src/main/java/ldts/Game.java
@@ -0,0 +1,48 @@
+package ldts;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private final TerminalScreen screen;
+    private final Arena arena;
+
+    public Game(int width, int heigt) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, heigt)).createTerminal();
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+
+        arena = new Arena(width, heigt);
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        arena.draw(screen.newTextGraphics());
+        screen.refresh();
+    }
+
+    private void processKey(KeyStroke key) {
+        arena.processKey(key);
+    }
+
+    public void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
+    }
+}
